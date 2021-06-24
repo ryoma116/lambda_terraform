4d49532ee34f0fb04f6667f4466caa660ab0fa77
ryoma116 <uma19880116>
2021-06-25 05:58:12 +0900

    :rocket: Add policy from lambda to dynamodb

--- main.tf
+++ main.tf
@@ -49,43 +49,51 @@ resource "aws_lambda_function" "get_unixtime" {
 resource "aws_iam_role" "lambda_iam_role" {
   name = "${var.system_name}_iam_role"
 
-  assume_role_policy = <<POLICY
+  assume_role_policy = jsonencode(
 {
-  "Version": "2012-10-17",
-  "Statement": [
+  Version: "2012-10-17",
+  Statement: [
     {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
+      Action: "sts:AssumeRole",
+      Principal: {
+        Service: "lambda.amazonaws.com"
       },
-      "Effect": "Allow",
-      "Sid": ""
+      Effect: "Allow",
+      Sid: ""
     }
   ]
-}
-POLICY
+})
 }
 
 # Policy
 resource "aws_iam_role_policy" "lambda_access_policy" {
   name   = "${var.system_name}_lambda_access_policy"
-  role   = "${aws_iam_role.lambda_iam_role.id}"
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "logs:CreateLogStream",
-        "logs:CreateLogGroup",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "arn:aws:logs:*:*:*"
-    }
-  ]
-}
-POLICY
+  role   = aws_iam_role.lambda_iam_role.id
+  policy = jsonencode({
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Effect: "Allow",
+        Action: [
+          "logs:CreateLogStream",
+          "logs:CreateLogGroup",
+          "logs:PutLogEvents"
+        ],
+        Resource: "arn:aws:logs:*:*:*"
+      },
+      {
+        Effect: "Allow",
+        Action: [
+          "dynamodb:Query",
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:Scan",
+          "dynamodb:Delete*",
+        ],
+        Resource: aws_dynamodb_table.dynamodb_table.arn
+      }
+    ]
+  })
 }
 
 resource "aws_lambda_permission" "apigw" {
