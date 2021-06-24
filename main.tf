93fa9ebe08ee99f135323f9dbdd230e62b0e2693
ryoma116 <uma19880116>
2021-06-25 04:07:38 +0900

    :rocket: Add API Gateway terraform

--- main.tf
+++ main.tf
@@ -87,3 +87,15 @@ resource "aws_iam_role_policy" "lambda_access_policy" {
 }
 POLICY
 }
+
+resource "aws_lambda_permission" "apigw" {
+   statement_id  = "AllowAPIGatewayInvoke"
+   action        = "lambda:InvokeFunction"
+   function_name = aws_lambda_function.get_unixtime.function_name
+   principal     = "apigateway.amazonaws.com"
+
+   # The "/*/*" portion grants access from any method on any resource
+   # within the API Gateway REST API.
+   source_arn = "${aws_api_gateway_rest_api.get_unixtime.execution_arn}/*/*"
+}
+
